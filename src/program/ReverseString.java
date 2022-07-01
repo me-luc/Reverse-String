785355e6a427b7335dfe722588ca2c4fb94d8687
Lucas A <44484333+me-luc@users.noreply.github.com>
2022-07-01 18:15:40 -0300

    mudanças

--- src/program/ReverseString.java
+++ src/program/ReverseString.java
@@ -1,5 +1,16 @@
 package program;
 
 public class ReverseString {
-
+	
+	public String getReverseString(String text) {
+		
+		StringBuilder reversedString = new StringBuilder();
+		
+		for(int i=0; i<text.length(); i++) {
+			reversedString.append(text.charAt(text.length() - i - 1));
+		}
+		
+		return reversedString.toString();
+	}
+	
 }
---
+++ src/program/main.java
@@ -0,0 +1,15 @@
+package program;
+
+import javax.swing.JOptionPane;
+
+public class main {
+
+	public static void main(String[] args) {
+		ReverseString conversor = new ReverseString();
+		String text = "";
+		
+		text = JOptionPane.showInputDialog(null, "Insira palavra a ser convertida:");
+		JOptionPane.showMessageDialog(null, conversor.getReverseString(text));
+	}
+
+}
